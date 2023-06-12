08d0586b4895090af789837075885e1fa872005f
nadanmohan <knadanmohanreddy@gmail.com>
2023-06-12 15:53:07 +0530

    anything

---
+++ src/Acting/Eating.java
@@ -0,0 +1,17 @@
+package Acting;
+
+public class Eating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("mahi");
+System.out.println("a");
+	}
+
+}
+
+
+
+
+
+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
